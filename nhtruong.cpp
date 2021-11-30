cf6cf307b492ed3b2c0bd738744d1be01aadc57a
truongjr <nguyentruongjr@gmail.com>
2021-11-30 22:35:19 +0700

    truong

---
+++ nhtruong.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ldb long double
+#define x first
+#define y second
+#define p pair<ll, ll>
+#define vt vector<ll> 
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define tp pair<ll, pair<ll, ll>>
+#define fi first
+#define se second.first
+#define th second.second
+
+const long long MOD = 1000000007;
+const long double PI = 3.141592653589793238462643383279502884;
+const long long INF = 1e18;
+
+void solve(){
+    
+}
+int main (){
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int q = 1;
+    cin >> q;
+    while(q--){
+        solve();
+    }
+	return 0;	
+}
