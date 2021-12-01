18c0ca9c0c4f924c8196ee18131dd3f5d883f44c
truongjr <nguyentruongjr@gmail.com>
2021-12-01 13:55:07 +0700

    fenwick_tree

---
+++ fenwick.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ldb long double
+#define x first
+#define y second
+#define p pair<ll, ll>
+#define vt vector<ll> 
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define tp pair<ll, pair<ll, ll>>
+#define fi first
+#define se second.first
+#define th second.second
+
+const long long MOD = 1000000007;
+const long double PI = 3.141592653589793238462643383279502884;
+const long long INF = 1e18;
+
+class Fenwick{
+    public:
+    int n;
+    vector<int> fw1, fw2;
+    Fenwick(int n){
+        this->n = n;
+        fw1.assign(n + 1, 0);
+        fw2.assign(n + 1, 0);
+    }
+    void updatePoint(vector<int> &fw, int u, int v){
+        int idx = u;
+        while (idx <= n){
+            fw[idx] += v;
+            idx += idx & (-idx);
+        }
+    }
+    void updateRange(int l, int r, int v){
+        updatePoint(fw1, l, (r - l + 1) * v);
+        updatePoint(fw1, r + 1, (n - r) * (-v));
+        updatePoint(fw2, l, v);
+        updatePoint(fw2, r + 1, v);
+    }
+    int getSum(vector<int> &fw, int index){
+        int idx = index, ans = 0;
+        while(idx > 0){
+            ans += fw[idx];
+            idx -= idx & (-idx);
+        }
+        return ans;
+    }
+    int prefixSum(int u){
+        return getSum(fw1, u) - getSum(fw2, u) * (n - u);
+    }
+    int rangeSum(int l, int r){
+        return prefixSum(r) - prefixSum(l - 1);
+    }
+    void check(){
+        for(auto it:fw1) cout << it << ' ';
+        cout << '\n';
+        for(auto it:fw2) cout << it << ' ';
+        cout << '\n';
+    }
+};
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    Fenwick fw(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    } 
+    for(int i = 0; i < n; i++){
+        fw.updatePoint(fw.fw1, i + 1, a[i]);
+    }
+    cout << fw.rangeSum(3, 4);
+    // for(auto it:fw.fw1) cout << it << ' ';
+}
+int main (){
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int q = 1;
+    // cin >> q;
+    while(q--){
+        solve();
+    }
+	return 0;	
+}
